4716a8033e93c18e06f01215bbd7253f1b63e2d6
achosikha <softdevelopment@yahoo.com>
2025-02-20 22:56:08 +0400

    Added Initializable and some other features and the task

--- src/main/java/com/example/vocabularytms/VocabularyController.java
+++ src/main/java/com/example/vocabularytms/VocabularyController.java
@@ -1,20 +1,34 @@
+// 1. Разобраться с кодом;
+// 2. Посмотреть как идет связь между бэком и графикой (вопрос контроллера)
+// 3. Привести все форму в нормальное состояние и все правильно расположить (можно просто через SceneBuilder)
+// 4. Добавить кнопку (четвертую) "Закрыть", которое будет ЗАКРЫВТЬ окно, т.е. прекращать программу;
+// 5. Все данные хранить в коллекции, сами выберите какую коллекцию хотите, Map
+// 6. Если в документе, где хранятся значения есть данные, все надо сохранить в коллекции и вывести мне на экран
+// 7. Если ничего нет, то просто пусто
+// 8. Нажав на кнопку Добавить я должен иметь возможность добавлять в конец
+// 9. Все добавленное также должно храниться уже в обновленном списке
+// 10. Нажав на кнопку Save весь новый список должен сохраниться в документе
+// 11. Если я нажму на кнопку Удалить, из списка должен быть удален элемент, также и коллекции и соответственно
+// его не должно быть в документ
+// 12. Каждый раз, когда я буду закрывать окно через кнопку Закрыть, он также должен сохранить все изменения в документе
+
 package com.example.vocabularytms;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class VocabularyController {
+public class VocabularyController implements Initializable {
     @FXML
     public ListView<String> listView;
-    public ArrayList<String> arrayListView;
     @FXML
     public Button delete;
     @FXML
@@ -24,22 +38,18 @@ public class VocabularyController {
     @FXML
     private Button add;
 
-    // Create File & Read / Write
-    private File file;
-    private FileInputStream read;
-    private FileOutputStream write;
+    ArrayList<String> storeValues;
 
+    // Create File & Read / Write
+    private final File file;
+    BufferedReader reader;
+
+    // Not everything can be by default created and sent to a constructor
+    // We must keep in mind timing and the way constructors are build and objects are created
     public VocabularyController(){
         listView = new ListView<>();
-
-        try{
-            file = new File("src/main/resources/com/example/vocabularytms/vocabulary.txtRT");
-        } catch (Exception e) {
-            System.out.println("ERRORRR");
-        }
-    }
-
-    private void fileCheck(){
+        file = new File("src/main/resources/com/example/vocabularytms/vocabulary.txt");
+        storeValues = new ArrayList<>();
     }
 
     @FXML
@@ -49,6 +59,8 @@ public class VocabularyController {
             System.out.println("Your field is either blank or empty...");
             return;
         }
+
+        listView.getItems().add(textValue.getText());
     }
 
     @FXML
@@ -59,11 +71,26 @@ public class VocabularyController {
     @FXML
     public void saveVocabulary(ActionEvent actionEvent) {
         // This method will save your data in a .TXT file
-        File file = new File("src/main/resources/com/example/vocabularytms/vocabulary.txt");
+        String[] words = new String[]{"Horse", "Dogs", "Cats", "Rabbits", "Camels"};
+        listView.getItems().addAll(words);
     }
 
-    // Read VOCABULARY.TXT
-    private void readVocabulary(){
-        // Читает документ/словарь и если он не пуст, то все выводит на экран
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // method which create a code once some object have been initialized
+        // Read document and save values to ArrayList
+        // Then add all ArrayList values to listview -> listview.getItems().addAll(_array);
+        try{
+            String line;
+            reader = new BufferedReader(new FileReader(file));
+
+            while((line = reader.readLine()) != null){
+                listView.getItems().add(line);
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("File was not found!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
